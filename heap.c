8397dfbd533b83b77f85f6d47124b6531e5ef351
Replit user <>
2023-05-09 23:26:05 +0000

    new try

--- heap.c
+++ heap.c
@@ -18,7 +18,7 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* pq){
-  if (pq == NULL || pq->size == NULL){
+  if (pq == NULL || pq->size == 0){
     return NULL;
   }
   return pq->heapArray[0].data;
