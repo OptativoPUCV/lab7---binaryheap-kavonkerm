edeedb1eefadbe58a3a08ef79c283c5d2872ec2a
Replit user <>
2023-05-11 16:28:22 +0000

    new try

--- heap.c
+++ heap.c
@@ -35,7 +35,7 @@ void heap_push(Heap* pq, void* data, int priority){
     i = (i - 1) / 2;
   }
   if (pq->size == pq->capac) {
-    nueva_Capacidad = (pq->capac * 2) + 1;
+    int nueva_Capacidad = (pq->capac * 2) + 1;
     //pq->capac = (pq->capac == 0) ? 1 : pq->capac * 2;
     pq->heapArray = realloc(pq->heapArray, nueva_Capacidad);
   }
